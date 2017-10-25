3522772177feaad06763f43721e9f027591dcb4c
Franklin Isalguez <franklingonzalez@neoscloudllc.com>
2017-10-25 13:27:47 -0400

    Fix raffle refresh when deleting segment

--- app/renderer.js
+++ app/renderer.js
@@ -80,6 +80,7 @@ RaffleApp.controller('RaffleCreateCtrl', function RaffleCreateCtrl($scope, $mdDi
   vm.deletePrize = function deletePrize(index) {
     if (vm.raffle.segments.length === 1) return;
     vm.raffle.segments.splice(index, 1);
+    vm.createPreview();
   };
 
   ipcRenderer.on('file-opened', function (event, file, content) {
@@ -210,81 +211,3 @@ RaffleApp.controller('RaffleCreateCtrl', function RaffleCreateCtrl($scope, $mdDi
     vm.myWheel = new Winwheel(raffle);
   };
 });
-
-// var myWheel = new Winwheel({
-//   'numSegments'    : 2,
-//   'outerRadius'    : 170,
-//   'segments'       :
-//   [
-//      {'fillStyle' : '#eae56f', 'text' : 'Prize 1', 'size': winwheelPercentToDegrees(1)},
-//      {'fillStyle' : '#89f26e', 'text' : 'Prize 2', 'size': winwheelPercentToDegrees(99)}
-//   ],
-//   'animation' :
-//   {
-//       'type'     : 'spinToStop',
-//       'duration' : 5,
-//       'spins'    : 8,
-
-//       // Remember to do something after the animation has finished specify callback function.
-//       'callbackFinished' : 'alertPrize()',
-
-//       // During the animation need to call the drawTriangle() to re-draw the pointer each frame.
-//       'callbackAfter' : 'drawTriangle()'
-//   }
-// });
-
-// // This function called after the spin animation has stopped.
-// function alertPrize()
-// {
-//   // Call getIndicatedSegment() function to return pointer to the segment pointed to on wheel.
-//   var winningSegment = myWheel.getIndicatedSegment();
-
-//   // Basic alert of the segment text which is the prize name.
-//   alert("You have won " + winningSegment.text + "!");
-
-//   myWheel = new Winwheel({
-//   'numSegments'    : 2,
-//   'outerRadius'    : 170,
-//   'segments'       :
-//   [
-//      {'fillStyle' : '#eae56f', 'text' : 'Prize 1', 'size': winwheelPercentToDegrees(99)},
-//      {'fillStyle' : '#89f26e', 'text' : 'Prize 2', 'size': winwheelPercentToDegrees(1)}
-//   ],
-//   'animation' :
-//   {
-//       'type'     : 'spinToStop',
-//       'duration' : 5,
-//       'spins'    : 8,
-
-//       // Remember to do something after the animation has finished specify callback function.
-//       'callbackFinished' : 'alertPrize()',
-
-//       // During the animation need to call the drawTriangle() to re-draw the pointer each frame.
-//       'callbackAfter' : 'drawTriangle()'
-//   }
-// });
-
-// }
-
-// // Function to draw pointer using code (like in a previous tutorial).
-// drawTriangle();
-
-// function drawTriangle()
-// {
-//   // Get the canvas context the wheel uses.
-//   var ctx = myWheel.ctx;
-
-//   ctx.strokeStyle = 'navy';     // Set line colour.
-//   ctx.fillStyle   = 'aqua';     // Set fill colour.
-//   ctx.lineWidth   = 2;
-//   ctx.beginPath();              // Begin path.
-//   ctx.moveTo(170, 5);           // Move to initial position.
-//   ctx.lineTo(230, 5);           // Draw lines to make the shape.
-//   ctx.lineTo(200, 40);
-//   ctx.lineTo(171, 5);
-//   ctx.stroke();                 // Complete the path by stroking (draw lines).
-//   ctx.fill();                   // Then fill.
-
-
-// }
-
